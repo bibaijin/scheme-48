1191174ed97108a68fa5454e083550732bd7c031
bibaijin <bibaijin@gmail.com>
2015-09-22 08:02:34 +0800

    initial commit

---
+++ listing3.1.hs
@@ -0,0 +1,34 @@
+module Main where
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+
+main :: IO ()
+main = do
+     (expr:_) <- getArgs
+     putStrLn expr
+     putStrLn (readExpr expr)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+                   Left err -> "No match: " ++ show err
+                   Right val -> "Found value"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+             
+parseString :: Parser LispVal
+parseString = do
+            char '"'
+            x <- many (noneOf "\"")
+            char '"'
+            return $ String x
